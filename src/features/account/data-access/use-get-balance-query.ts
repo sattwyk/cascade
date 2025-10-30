3a8925f9b42c71f5859e20879b4ea8d549d4041e
Sattwik Biswal <sattwyk@gmail.com>
2025-10-30 16:29:24 +0530

    feat(account): add mutation for requesting developer token top-up with error handling

--- src/features/account/data-access/use-get-balance-query.ts
+++ src/features/account/data-access/use-get-balance-query.ts
@@ -5,12 +5,13 @@ import { useSolana } from '@/components/solana/use-solana';
 
 import { useGetBalanceQueryKey } from './use-get-balance-query-key';
 
-export function useGetBalanceQuery({ address }: { address: Address }) {
+export function useGetBalanceQuery({ address, enabled = true }: { address: Address; enabled?: boolean }) {
   const { client } = useSolana();
 
   return useQuery({
     retry: false,
     queryKey: useGetBalanceQueryKey({ address }),
     queryFn: () => client.rpc.getBalance(address).send(),
+    enabled,
   });
 }
