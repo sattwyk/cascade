d0651b5893340353316059f39fcdc526978a56c9
Sattwik Biswal <sattwyk@gmail.com>
2026-02-21 01:37:41 +0530

    ui: enforce sidebar separator auto width

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -333,7 +333,7 @@ function SidebarSeparator({ className, ...props }: React.ComponentProps<typeof S
     <Separator
       data-slot="sidebar-separator"
       data-sidebar="separator"
-      className={cn('mx-2 w-auto bg-sidebar-border', className)}
+      className={cn('mx-2 w-auto! bg-sidebar-border', className)}
       {...props}
     />
   );
